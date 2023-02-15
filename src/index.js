0cf55ebfcd828df14eee9d9eb8b29c9c822536df
SrElkenn <125503653+SrElkenn@users.noreply.github.com>
2023-02-15 16:43:21 -0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,24 @@
+let numero = 1;
+
+{
+  let numero = 2;
+  console.log("dentro = ", numero);
+}
+
+console.log("fora = ", numero);
+
+// Declarando Variável com var
+
+var numero1 = 1;
+
+{
+  var numero1 = 2;
+  console.log("dentro1 = ", numero1);
+}
+
+console.log("fora1 = ", numero1);
+
+// Declarando uma Constante
+
+// const numero2 = 10
+// numero2 = 11
